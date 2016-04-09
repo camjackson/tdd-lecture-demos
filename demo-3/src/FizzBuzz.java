b41016d0da7e0f48f32d57a25ad4af5c6ae0731c
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-04-09 17:51:59 +1000

    Demo 3, step 10, main

--- demo-3/src/FizzBuzz.java
+++ demo-3/src/FizzBuzz.java
@@ -21,4 +21,8 @@ public class FizzBuzz {
         String resultWithNoTrailingComma = result.substring(0, result.length() - 2);
         return resultWithNoTrailingComma;
     }
+
+    public static void main(String[] args) {
+        System.out.println(new FizzBuzz().fizzBuzzSequence());
+    }
 }
