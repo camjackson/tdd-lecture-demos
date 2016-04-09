52d59871aa53b63e8ca21619273f607578014b10
Pamela Rucinque <prucinqu@thoughtworks.com>
2016-04-09 16:47:42 +1000

    Step by step

--- demo-3/src/FizzBuzz.java
+++ demo-3/src/FizzBuzz.java
@@ -1,29 +1,6 @@
 public class FizzBuzz {
+
     String fizzBuzz(int number) {
-        String result = String.valueOf(number);
-
-        if(number % 15 == 0){
-            result = "FizzBuzz";
-        } else if (number % 3 == 0) {
-            result = "Fizz";
-        } else if (number % 5 == 0) {
-            result = "Buzz";
-        }
-        return result;
-    }
-
-    String fizzBuzzSequence() {
-        String result = "";
-        for (int i = 1; i <= 50; i++) {
-            result += fizzBuzz(i) + ", ";
-        }
-
-        String resultWithNoTrailingComma = result.substring(0, result.length() - 2);
-
-        return resultWithNoTrailingComma;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new FizzBuzz().fizzBuzzSequence());
+        return "1";
     }
 }
--- demo-3/test/FizzBuzzTest.java
+++ demo-3/test/FizzBuzzTest.java
@@ -3,33 +3,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
-    @Test
-    public void itReturnsTheNumberAsStringIfNotDivisibleByThreeOrFive() throws Exception {
-        assertEquals("-1", new FizzBuzz().fizzBuzz(-1));
-    }
 
     @Test
-    public void itReturnsFizzIfDivisibleByThree() throws Exception {
-        assertEquals("Fizz", new FizzBuzz().fizzBuzz(3));
-    }
-
-    @Test
-    public void itReturnsBuzzIfDivisibleByFive() throws Exception {
-        assertEquals("Buzz", new FizzBuzz().fizzBuzz(5));
-    }
-
-    @Test
-    public void itReturnsFizzBuzzIfDivisibleByThreeAndFive() throws Exception {
-        assertEquals("FizzBuzz", new FizzBuzz().fizzBuzz(15));
-    }
-
-    @Test
-    public void itStartsWithTheRightSequence() throws Exception {
-        assert(new FizzBuzz().fizzBuzzSequence().startsWith("1, 2, Fizz, 4, Buzz"));
-    }
-    @Test
-    public void itEndsWithTheRightSequence() throws Exception {
-        System.out.println(new FizzBuzz().fizzBuzzSequence());
-        assert(new FizzBuzz().fizzBuzzSequence().endsWith("Fizz, 49, Buzz"));
+    public void itReturnsTheNumberAsStringIfNotDivisibleByThreeOrFive() {
+        assertEquals("1", new FizzBuzz().fizzBuzz(1));
     }
 }
