dcfa368ead386b08dcbc66455d6404a77e767065
Pam Rucinque <pamela.rucinque@gmail.com>
2017-03-26 11:17:48 +1100

    Adding step by step for Demo4

--- demo-4-and-5/test/AssignmentMarkerTest.java
+++ demo-4-and-5/test/AssignmentMarkerTest.java
@@ -3,13 +3,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AssignmentMarkerTest {
+
+    class NeverPlagiarised extends PlagiarismService {
+        boolean assignmentIsPlagiarised(Assignment assignment) {
+            return false;
+        }
+    }
+
     @Test
-    public void itDoesSomething() throws Exception {
-        Assignment assignment = new Assignment(true, true);
-        PlagiarismService plagiarismService = new PlagiarismService();
+    public void itScores5WhenNotPlagiarisedAndHalfCorrect() throws Exception {
+        Assignment assignment = new Assignment(false, true);
+        PlagiarismService neverPlagiarised = new NeverPlagiarised();
 
-        int mark = new AssignmentMarker().markAssignment(assignment, plagiarismService);
-
-        assertEquals(-1, mark);
+        int mark = new AssignmentMarker().markAssignment(assignment, neverPlagiarised);
+        assertEquals(5, mark);
     }
 }
