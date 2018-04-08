886a64fc4eb51667c0057e8d3958ebd4c2bd7046
Pam Rucinque <pamela.rucinque@gmail.com>
2018-04-09 07:35:55 +1000

    Add real implementation for Demo 1

--- demo-1-and-2/test/DividerTest.java
+++ demo-1-and-2/test/DividerTest.java
@@ -4,7 +4,27 @@ import static org.junit.Assert.*;
 
 public class DividerTest {
     @Test
-    public void itDoesSomething() throws Exception {
-        assertEquals(-1, new Divider().divide(-1, -1));
+    public void dividingByZeroReturnsZero() {
+        assertEquals(0, new Divider().divide(4, 0));
+    }
+
+    @Test
+    public void dividingANumberByItselfReturnsOne() {
+        assertEquals(1, new Divider().divide(5, 5));
+    }
+
+    @Test
+    public void dividingANumberByOneReturnsTheSameNumber() {
+        assertEquals(29, new Divider().divide(29, 1));
+    }
+
+    @Test
+    public void itRoundsUsingFloor() {
+        assertEquals(1, new Divider().divide(13, 7));
+    }
+
+    @Test
+    public void dividingByTwoReturnsHalfOfTheEnumerator() {
+        assertEquals(3, new Divider().divide(6, 2));
     }
 }
