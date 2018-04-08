34440e7f4fab94cc622edcd95b698ededbc20c98
Pam Rucinque <pamela.rucinque@gmail.com>
2018-04-09 07:33:18 +1000

    Removing this step. There is not enought time to show bad written tests during the lecture.
    Revert "Add implementation for Demo 1"
    
    This reverts commit cb61fa7071d0b75fef14d306bbe47723b13c0984.

--- demo-1-and-2/test/DividerTest.java
+++ demo-1-and-2/test/DividerTest.java
@@ -4,32 +4,7 @@ import static org.junit.Assert.*;
 
 public class DividerTest {
     @Test
-    public void dividingSixByTwoReturnsThree() {
-        assertEquals(3, new Divider().divide(6, 2));
-    }
-
-    @Test
-    public void dividingFiveByOneReturnsFive() {
-        assertEquals(5, new Divider().divide(5, 1));
-    }
-
-    @Test
-    public void dividingSevenBySevenReturnsOne() {
-        assertEquals(1, new Divider().divide(7, 7));
-    }
-
-    @Test
-    public void dividingEightBySevenReturnsOne() {
-        assertEquals(1, new Divider().divide(8, 7));
-    }
-
-    @Test
-    public void dividingSevenByEightReturnsZero() {
-        assertEquals(0, new Divider().divide(7, 8));
-    }
-
-    @Test
-    public void dividingOneByZeroReturnsZero() {
-        assertEquals(0, new Divider().divide(1, 0));
+    public void itDoesSomething() throws Exception {
+        assertEquals(-1, new Divider().divide(-1, -1));
     }
 }
