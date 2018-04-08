98076ec25815446d2f66a7ec3f7b50c2c4676f65
Pam Rucinque <pamela.rucinque@gmail.com>
2018-04-09 07:44:20 +1000

    Start of Demo 2 - Class implementation removed, show what happens when tests written first

--- demo-1-and-2/src/Divider.java
+++ demo-1-and-2/src/Divider.java
@@ -1,8 +1,5 @@
 public class Divider {
     int divide(int numerator, int denominator) {
-        if (denominator == 0) {
-            return 0;
-        }
-        return numerator / denominator;
+        return 55;
     }
 }
